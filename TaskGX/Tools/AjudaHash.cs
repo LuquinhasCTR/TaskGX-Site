d7c1798e646aa51d0b8ac8a6a635e6509ac084e7
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-02 11:59:47 +0000

    Atualiza o AjudaHash.cs.

---
+++ TaskGX/Tools/AjudaHash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace TaskGX.Tools
+{
+    public static class AjudaHash
+    {
+        public static string GerarHashSenha(string senha)
+        {
+            if (string.IsNullOrWhiteSpace(senha))
+                throw new ArgumentException("Senha inválida.");
+
+            return BCrypt.Net.BCrypt.HashPassword(senha);
+        }
+
+        public static bool VerificarSenha(string senhaDigitada, string hashArmazenado)
+        {
+            return BCrypt.Net.BCrypt.Verify(senhaDigitada, hashArmazenado);
+        }
+    }
+}
