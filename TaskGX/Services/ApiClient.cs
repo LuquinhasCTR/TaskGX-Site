28913a2890e24696ffdad75d00fabf57d9e1ea52
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2026-02-06 11:13:57 +0000

    Adiciona as diretivas HTTP using e move o ApiClient para Web.Services.

--- TaskGX/Services/ApiClient.cs
+++ TaskGX/Services/ApiClient.cs
@@ -1,4 +1,10 @@
 ﻿namespace TaskGX.Services
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace TaskGX.Web.Services
 {
     public class ApiClient
     {
